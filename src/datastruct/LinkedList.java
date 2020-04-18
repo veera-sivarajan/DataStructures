51ce8a4b1240af5afd25fdde391ee70ce3908103
Veera Sivarajan <sveera.2001@gmail.com>
2020-04-18 14:06:45 -0400

    Added Size and Add Methods

--- src/datastruct/LinkedList.java
+++ src/datastruct/LinkedList.java
@@ -16,8 +16,26 @@ public class LinkedList<T> {
   }
   
   private int getSizeHelper(Node<T> currNode) {
-    
-    while(currNode != null) {
-      return 1 + currNode.getLink();
-    } 
+    if(currNode == null) 
+      return 0;
+    return 1 + getSizeHelper(currNode.getLink()); 
+  }
+  
+  public void addToTail(Node<T> link) {
+    Node<T> tail = head;
+    while(tail != null) 
+      tail = tail.getLink();
+    tail.setLink(link);
+  }
+
+  public void addToHead(Node<T> link) {
+    link.setLink(head);
+    this.head = link;
+  }
+  
+  public static void main(String[] args) {
+    LinkedList list = new LinkedList<T>();
+    Node ele1 = new Node<T>(5, null);
+    list.addToTail(ele1);
+  }  
 }
