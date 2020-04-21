ad065ad8abbf2531d343a0e2c782e8077428ed58
Veera Sivarajan <sveera.2001@gmail.com>
2020-04-21 02:44:05 -0400

    Fixed addToTail

--- src/datastruct/LinkedList.java
+++ src/datastruct/LinkedList.java
@@ -3,39 +3,59 @@ package datastruct;
 import datastruct.Node;
 
 public class LinkedList<T> {
-  private Node<T> head;  
+  private Node<Integer> head;  
   
   public LinkedList() {
-    this.head = null;
+    this.head = new Node<Integer>(0, null);
   }
   
-  public int getSize() {
+  public int size() {
     if(head == null) 
       throw new NullPointerException("Empty List");
     return getSizeHelper(head);
   }
   
-  private int getSizeHelper(Node<T> currNode) {
+  private int getSizeHelper(Node<Integer> currNode) {
     if(currNode == null) 
       return 0;
     return 1 + getSizeHelper(currNode.getLink()); 
   }
   
-  public void addToTail(Node<T> link) {
-    Node<T> tail = head;
-    while(tail != null) 
-      tail = tail.getLink();
-    tail.setLink(link);
+  public void addToTail(Node<Integer> link) {
+    System.out.println("Entering addToTail");
+    if(head.getLink() == null) { 
+      head = link;
+      System.out.println("Adding to Head");
+    }
+    else {
+      System.out.println("Adding to tail using while loop");
+      Node<Integer> tail = head;
+      while(tail.getLink() != null)
+	tail = tail.getLink();
+      tail.setLink(link);
+    } 
   }
 
-  public void addToHead(Node<T> link) {
+  public void addToHead(Node<Integer> link) {
     link.setLink(head);
     this.head = link;
   }
   
+  public void printList() {
+    Node<Integer> tail = head;
+    while(tail != null) {
+      System.out.println("Data: " + tail.getData());  
+      tail = tail.getLink();
+    }
+  }
+  
   public static void main(String[] args) {
-    LinkedList list = new LinkedList<T>();
-    Node ele1 = new Node<T>(5, null);
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    Node<Integer> ele1 = new Node<Integer>(4, null);
+    Node<Integer> ele2 = new Node<Integer>(5, null);
     list.addToTail(ele1);
+    list.addToHead(ele2);
+    list.printList();
+    System.out.println("Size: " + list.size()); 
   }  
 }
